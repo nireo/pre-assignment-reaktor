98fc76586878cd061cbcd95c4575bed0fe100c0a
nireo <eemil.lehtikangas1@gmail.com>
2020-02-26 19:04:18 +0200

    added button styling

--- data.js
+++ data.js
@@ -1,2 +1,39 @@
-// parsing the data happens here
-export const getData = () => {};
+import { setObjects } from "./index";
+
+let tempObjects = [];
+
+const parseSingle = item => {
+  const objectTemplate = {
+    name: "",
+    depends: "",
+    description: ""
+  };
+
+  objectTemplate.name = item.split("Status")[0].split("Essential")[0];
+
+  let depends = item.split("Depends:")[1];
+  depends = depends ? depends.split(", ") : [];
+  if (depends !== []) {
+    depends = depends.map(d => d.slice(0, d.indexOf("(")).trim());
+    depends = depends.filter(d => !d.includes(" "));
+  }
+
+  objectTemplate.depends = depends;
+
+  objectTemplate.description = item
+    .split("Description: ")[1]
+    .split("Homepage:")[0]
+    .split("Original-Maintainer:")[0];
+
+  objects = objects.concat(objectTemplate);
+};
+
+export const parseMain = data => {
+  console.log("hello");
+  const withOutNewLines = data.replace(/(\r\n|\n|\r)/gm, "");
+  let arrays = withOutNewLines.split("Package: ");
+  for (let i = 1; i < arrays.length; i++) {
+    parseSingle(arrays[i]);
+  }
+  setObjects(tempObjects);
+};
--- index.js
+++ index.js
@@ -3,84 +3,87 @@ let selectedItem = null;
 
 const parseData = item => {
   const objectTemplate = {
-    name: "",
-    depends: "",
-    description: ""
+    name: '',
+    depends: '',
+    description: ''
   };
 
-  objectTemplate.name = item.split("Status")[0].split("Essential")[0];
+  objectTemplate.name = item.split('Status')[0].split('Essential')[0];
 
-  let depends = item.split("Depends:")[1];
-  depends = depends ? depends.split(", ") : [];
+  let depends = item.split('Depends:')[1];
+  depends = depends ? depends.split(', ') : [];
   if (depends !== []) {
-    depends = depends.map(d => d.slice(0, d.indexOf("(")).trim());
-    depends = depends.filter(d => !d.includes(" "));
+    depends = depends.map(d => d.slice(0, d.indexOf('(')).trim());
+    depends = depends.filter(d => !d.includes(' '));
   }
 
   objectTemplate.depends = depends;
 
   objectTemplate.description = item
-    .split("Description: ")[1]
-    .split("Homepage:")[0]
-    .split("Original-Maintainer:")[0];
+    .split('Description: ')[1]
+    .split('Homepage:')[0]
+    .split('Original-Maintainer:')[0];
 
   objects = objects.concat(objectTemplate);
 };
 
 const checkSelectedItem = () => {
-  let packages = document.getElementById("packages");
-  packages.style.display = "none";
+  let packages = document.getElementById('packages');
+  packages.style.display = 'none';
   if (selectedItem === null) {
     // if no item is selected, display them all
-    packages.style.display = "";
+    packages.style.display = '';
     return;
   }
 
-  let single = document.getElementById("single-item");
+  let single = document.getElementById('single-item');
 
-  document.getElementById("go-back-button").onclick = () => {
+  document.getElementById('go-back-button').onclick = () => {
     selectedItem = null;
-    single.style.display = "none";
+    single.style.display = 'none';
     checkSelectedItem();
   };
-  single.style.display = "";
+  document.getElementById('go-back-button').className += 'button';
+  single.style.display = '';
 
-  document.getElementById("title").innerHTML = selectedItem.name;
-  document.getElementById("description").innerHTML = selectedItem.description;
+  document.getElementById('title').innerHTML = selectedItem.name;
+  document.getElementById('description').innerHTML = selectedItem.description;
 
   // clear the old dependency field
-  document.getElementById("depends").innerHTML = "";
+  document.getElementById('depends').innerHTML = '';
   selectedItem.depends.forEach(depend => {
-    let button = document.createElement("button");
+    let button = document.createElement('button');
+    button.className += 'button';
     button.innerHTML = depend;
     button.onclick = () => {
       selectedItem = objects.find(i => i.name === depend);
       checkSelectedItem();
     };
 
-    document.getElementById("depends").appendChild(button);
+    document.getElementById('depends').appendChild(button);
   });
 };
 
-fetch("./status-data.txt")
+fetch('./status-data.txt')
   .then(r => r.text())
   .then(data => {
-    const withOutNewLines = data.replace(/(\r\n|\n|\r)/gm, "");
-    let arrays = withOutNewLines.split("Package: ");
+    const withOutNewLines = data.replace(/(\r\n|\n|\r)/gm, '');
+    let arrays = withOutNewLines.split('Package: ');
     for (let i = 1; i < arrays.length; i++) {
       parseData(arrays[i]);
     }
 
     document.getElementById(
-      "counter"
+      'counter'
     ).innerHTML = `${objects.length} packages loaded`;
     objects.forEach(item => {
-      let button = document.createElement("button");
+      let button = document.createElement('button');
+      button.className += 'button';
       button.innerHTML = item.name;
       button.onclick = () => {
         selectedItem = item;
         checkSelectedItem();
       };
-      document.getElementById("packages").appendChild(button);
+      document.getElementById('packages').appendChild(button);
     });
   });
